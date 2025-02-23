3778837910e7f073719983719eadaac0a6d256ac
OdysseyOfTroy <TroyMatthewsHong@gmail.com>
2025-02-23 01:04:06 +0000

    allow deserialising from lowercase to enable back compatibility

--- src/enums/traits.rs
+++ src/enums/traits.rs
@@ -5,11 +5,17 @@ use serde::{Deserialize, Serialize};
 #[derive(Debug, PartialEq, Clone, Copy, Serialize,Deserialize)]
 
 pub enum Traits {
+    #[serde(rename = "strength")]
     Strength,
+    #[serde(rename = "discipline")]
     Discipline,
+    #[serde(rename = "constitution")]
     Constitution,
+    #[serde(rename = "intelligence")]
     Intelligence,
+    #[serde(rename = "sense")]
     Sense,
+    #[serde(rename = "will")]
     Will
 }
 
